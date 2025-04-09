99f74015ec88af087b19732f17224d93b9b00f85
bu-hl <bjh0921@gmail.com>
2025-04-09 18:58:34 +0900

    fix: remove clean network sripts

--- basic-network/scripts/cleanNetwork.sh
+++ basic-network/scripts/cleanNetwork.sh
@@ -3,15 +3,15 @@
 echo "CA 컨테이너 및 노드 컨테이너 삭제"
 cd $GOPATH/src/dev-mode/basic-network/docker
 docker-compose -f docker-compose-ca.yaml down --volumes
-docker-compose -f docker-compose-test-net.yaml -f docker-compose-test-net.yaml down --volumes --remove-orphans
+docker-compose -f docker-compose-test-net.yaml down --volumes --remove-orphans
 
-docker rm $(docker ps -aq)
-docker rmi $(docker images dev-* -q)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images dev-* -q)
 docker volume prune -f
 docker network prune -f
 
 echo "프로젝트 인증서 및 트랜잭션 정보 삭제"
-## 프로젝트 인증서 및 트랜잭션 정보 삭제
+## 프로젝트 인증서 및 트랜잭션 정보 삭제s
 cd $GOPATH/src/dev-mode/basic-network
 rm -fr channel-artifacts
 rm -fr organizations/ordererOrganizations
